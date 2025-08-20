eb47513ef156d827f0953b3d04b739b7599e1be6
JSRxLogan <jsrxloganxmastermind666@gmail.com>
2025-08-20 23:39:29 +0530

    fixed toolstatusbar responsiviness

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,55 +1,63 @@
-import {useEffect,useState} from 'react'
-import {Editor} from '@tinymce/tinymce-react'
-import {Controller} from 'react-hook-form'
+import { Controller } from "react-hook-form";
+import { Editor } from "@tinymce/tinymce-react";
 
-function RTE({name,label, control,defaultValue=""}) {
+function RTE({ name, label, control, defaultValue = "" }) {
   return (
-    <div className='w-full'>
-
-        {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
-        <Controller 
+    <div className="w-full max-w-full overflow-hidden">
+      {label && <label className="inline-block mb-1 pl-1">{label}</label>}
+      <Controller
         name={name || "content"}
         control={control}
-        render={({field : {onChange}})=>(
-            <Editor 
+        render={({ field: { onChange } }) => (
+          <Editor
             initialValue={defaultValue}
-            apiKey='5wxiz1nyh8oi41r9lj9dbfgjrv9ygodm0f9yyl4bn1x244uu'
-        init={{
-            height: 500,
-            menubar: true,
-            plugins: [
-                "image",
-                "advlist",
-                "autolink",
-                "lists",
-                "link",
-                "image",
-                "charmap",
-                "preview",
-                "anchor",
-                "searchreplace",
-                "visualblocks",
-                "code",
-                "fullscreen",
-                "insertdatetime",
-                "media",
-                "table",
-                "code",
-                "help",
-                "wordcount",
-                "anchor",
-            ],
-            toolbar:
-            "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
-        }}
-        onEditorChange={onChange}
-            />
+            apiKey="5wxiz1nyh8oi41r9lj9dbfgjrv9ygodm0f9yyl4bn1x244uu"
+            init={{
+  menubar: true,
+  min_height: 300,
+  width: "100%",
+  resize: true,
+  toolbar_mode: "sliding",
+  plugins: [
+    "image",
+    "advlist",
+    "autolink",
+    "lists",
+    "link",
+    "charmap",
+    "preview",
+    "anchor",
+    "searchreplace",
+    "visualblocks",
+    "code",
+    "fullscreen",
+    "insertdatetime",
+    "media",
+    "table",
+    "help",
+    "wordcount",
+  ],
+  toolbar:
+    "undo redo | blocks | image | bold italic forecolor | " +
+    "alignleft aligncenter alignright alignjustify | " +
+    "bullist numlist outdent indent | removeformat | help",
+
+  // ✅ Custom CSS for statusbar
+  content_style: `
+    body { font-family:Helvetica,Arial,sans-serif; font-size:14px }
+    .tox-statusbar { min-height: 40px !important; } 
+    .tox-statusbar__path, .tox-statusbar__wordcount {
+      line-height: 40px !important; /* centers text vertically */
+    }
+  `
+}}
+
+            onEditorChange={onChange}
+          />
         )}
-        />
-      
+      />
     </div>
-  )
+  );
 }
 
-export default RTE
+export default RTE;
