c3359a0c00392e52f72f42807c6cb314206cbce5
JSRxLogan <jsrxloganxmastermind666@gmail.com>
2025-08-20 22:59:12 +0530

    fixed AllPost

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,67 +1,76 @@
-import React from 'react'
-import { PostCard } from '../components/index'  
-import {Container} from '../components/index.js'
-import { useEffect , useState} from 'react'
+import React, { useEffect, useState } from 'react'
+import { PostCard, Container } from '../components/index'
 import appwriteService from '../appwrite/config.js'
 import { useSelector } from 'react-redux'
 
 function AllPosts() {
+  const authStatus = useSelector((state) => state.auth.status)
+  const [posts, setPosts] = useState([])
+  const [error, setError] = useState(null)
 
-    const authStatus = useSelector((state) => state.auth.status)
-    const [posts, setPosts] = useState([])
-    const [error, setError] = useState(null)
-
-    useEffect(() => {
-      setError(null)
-      if(authStatus)
-      {
-        appwriteService.getAllposts([])
+  useEffect(() => {
+    setError(null)
+    if (authStatus) {
+      appwriteService
+        .getAllposts([])
         .then((response) => {
           setPosts(response.documents)
         })
-        .catch((error) => {
-          setError("Failed to fetch posts. Please try again later.")
+        .catch(() => {
+          setError('Failed to fetch posts. Please try again later.')
         })
-      }
-      else{
-        setPosts([])
-        setError("You need to login to see posts")  
-      }
-    }, [authStatus])
-    
-    // console.log("AllPosts",posts)
+    } else {
+      setPosts([])
+      setError('You need to login to see posts')
+    }
+  }, [authStatus])
 
-    if(!authStatus) {
-      return(
-        <div className='w-full py-8'>
+  if (!authStatus) {
+    return (
+      <div className="w-full py-8">
         <Container>
-            <h1 className='text-2xl font-bold text-red-500 text-center'>You need to login to see posts</h1>
+          <h1 className="text-2xl font-bold text-red-500 text-center">
+            You need to login to see posts
+          </h1>
         </Container>
-        </div>
-      )
-    }
+      </div>
+    )
+  }
 
-    if (error) {
-      return   <p className="text-red-600 mt-8 text-center">{error}</p>
-    }
+  if (error) {
+    return <p className="text-red-600 mt-8 text-center">{error}</p>
+  }
+
+  console.log(posts)
 
   return (
-<div className='w-full py-8'>
-        <Container>
-            <div className='flex flex-wrap'>
-               {posts && posts.length > 0 ? (
-                posts.map((post) => (
-                  console.log("post",post),
-                    // <PostCard key={post.$id} post={post} />
-                    <PostCard key={post.$id} $id={post.$id}
-                    title={post.title}
-                    image={post.image}/>
-                ))
-               ): <p>No posts available</p>}
-            </div>
-            </Container>
+    <div className="w-full py-8">
+      <Container>
+        {posts && posts.length > 0 ? (
+          <div
+            className="
+              grid 
+              grid-cols-1 
+              sm:grid-cols-2 
+              lg:grid-cols-3 
+              xl:grid-cols-4 
+              gap-6
+            "
+          >
+            {posts.map((post) => (
+              <PostCard
+                key={post.$id}
+                $id={post.$id}
+                title={post.title}
+                image={post.image}
+              />
+            ))}
+          </div>
+        ) : (
+          <p className="text-center text-gray-500">No posts available</p>
+        )}
+      </Container>
     </div>
-
   )
 }
 
